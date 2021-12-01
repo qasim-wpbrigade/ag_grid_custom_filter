7ff520ab64d9e4a267975bcd5f044fb4bab1ecf1
qasim-wpbrigade <87133962+qasim-wpbrigade@users.noreply.github.com>
2021-12-01 12:20:53 +0500

    Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
+const columnDefs = [
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model", sort: "desc" },
+    { headerName: "Price", field: "price" },
+];
+
+const rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+];
+
+const defaultColDef = {
+    editable: false,
+    flex: 1,
+    filter: false,
+    sortable: true,
+};
+
+const App = () => {
+    const [data, setdata] = useState([]);
+    const [filterData, setfilterData] = useState([]);
+    const [filter, setfilter] = useState({
+        make: "",
+        model: "",
+        price: "",
+        filtering: false,
+    });
+    useEffect(async () => {
+        if (filter.filtering) {
+            const fd = await data.filter((i) => {
+                return (
+                    i.make.toLowerCase().includes(filter.make) &&
+                    i.model.includes(filter.model) &&
+                    i?.price.toString().includes(filter.price.toString())
+                );
+            });
+            setfilterData(fd);
+            setfilter({ ...filter, filtering: false });
+        }
+    }, [filter]);
+    useEffect(() => {
+        setdata(rowData);
+        setfilterData(rowData);
+    }, []);
+    return (
+        <div className="App">
+            Make:&nbsp;
+            <input
+                type="text"
+                name="make"
+                id="make"
+                value={filter.make}
+                onChange={(e) =>
+                    setfilter({
+                        ...filter,
+                        make: e.target.value.toLowerCase(),
+                        filtering: true,
+                    })
+                }
+            />
+            Model:&nbsp;
+            <input
+                type="text"
+                name="model"
+                id="model"
+                value={filter.model}
+                onChange={(e) =>
+                    setfilter({
+                        ...filter,
+                        model: e.target.value.toLowerCase(),
+                        filtering: true,
+                    })
+                }
+            />
+            Price:&nbsp;
+            <input
+                type="number"
+                name="price"
+                id="price"
+                value={filter.price}
+                onChange={(e) =>
+                    setfilter({
+                        ...filter,
+                        price: e.target.value.toLowerCase(),
+                        filtering: true,
+                    })
+                }
+            />
+            <br></br>
+            <br></br>
+            <div
+                className="ag-theme-balham margin10"
+                style={{ height: "200px", width: "600px" }}
+            >
+                <AgGridReact
+                    columnDefs={columnDefs}
+                    rowData={filterData}
+                    defaultColDef={defaultColDef}
+                ></AgGridReact>
+            </div>
+        </div>
+    );
+};
 
 export default App;
